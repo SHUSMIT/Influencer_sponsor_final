9275031e9d0974e69021ae2e5fe702175c0ab043
SHUSMIT <166546567+SHUSMIT@users.noreply.github.com>
2025-10-29 18:50:23 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
-# /NoctiWave/run.py
+# /run.py
 
-from app import database_creator
-from app import app_creator
+from app import app_creator, database_creator
 
+# 1. Create the 'app' object so Gunicorn can import it
+app = app_creator()
+
+# 2. Create the database tables
+database_creator(app)
+
+# 3. This part is now just for running locally
 if __name__ == "__main__":
-    database_creator()
-    app_creator()
+    app.run(debug=True)
